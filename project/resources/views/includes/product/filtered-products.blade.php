d8526f9a01d98bd0fd840d95290290f1fe47f71b
unknown <tamim008bd@gmail.com>
2020-04-28 11:08:24 -0700

    'updated'\

--- project/resources/views/includes/product/filtered-products.blade.php
+++ project/resources/views/includes/product/filtered-products.blade.php
@@ -1,57 +1,65 @@
-			@if (count($prods) > 0)
-				<div class="row">
-						@foreach ($prods as $key => $prod)
-									<div class="col-lg-4 col-md-4 col-6" style="padding-left:10px;padding-right:10px;">
+ @if (count($prods) > 0)
+<div class="row">
+    @foreach ($prods as $key => $prod)
+    <div class="col-lg-4 col-md-4 col-6" style="padding-left:10px;padding-right:10px;">
 
-										<div class="product-item">
-											<div class="thumbnail">
-											<div class="extra-list top-icon-bar">
-												<ul>	
-													<li>
-														<span class="quick-view" rel-toggle="tooltip" title="" href="javascript:;" data-href="http://shotovag.com/shop/item/quick/view/124" data-toggle="modal" data-target="#comment-log-reg" data-placement="right" data-original-title="Add To Cart"> <i class="icofont-cart"></i>
+        <div class="product-item">
+            <div class="thumbnail">
+                <div class="extra-list top-icon-bar">
+                    <ul>
+                        <li>
+
+                            <span class="quick-view add-new-cart  " rel-toggle="tooltip" title="" href="javascript:;" data-href="{{route('product.cart.add',$prod->id)}}" data-original-title="Add To Cart"> <i class="icofont-cart"></i>
 														</span>
-													</li>
-													<li>
-														<span href="javascript:;" class="add-to-compare" data-href="http://shotovag.com/shop/item/compare/add/124" data-toggle="tooltip" data-placement="right" title="" data-original-title="Compare">
+
+                        </li>
+                        <li>
+                            <span href="javascript:;" class="add-to-compare" data-href="{{route('product.compare.add',$prod->id)}}" data-toggle="tooltip" data-placement="right" title="" data-original-title="Compare">
 															<i class="icofont-exchange"></i>
 														</span>
-													</li>
-													<li>
-														<span href="javascript:;" rel-toggle="tooltip" title="" data-toggle="modal" id="wish-btn" data-target="#comment-log-reg" data-placement="right" data-original-title="Add To Wishlist">
+                        </li>
+                        <li>
+                            @if(Auth::guard('web')->check())
+                            <span class="wishlist-new" href="javascript:;" data-href="{{ route('user-wishlist-add',$prod->id) }}" rel-toggle="tooltip" title="" id="wish-btn" data-placement="right" data-original-title="Add To Wishlist">
 															<i class="icofont-heart-alt"></i>
-														</span> 
-													</li>
-												</ul>
-    										 </div>
-												<a href="{{ route('front.product', $prod->slug) }}">
-													<div class="product-thum-img">
-														<a href="{{ route('front.product', $prod->slug) }}"><img  src="{{ $prod->photo ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt=""></a>
-													</div>
-												</a>
-												<h5 class="mt-2">{{$prod->name}} </h5>
-												<div class="local-info d-flex flex-row flex-wrap">
-												<div class="d-flex" style="margin-top:5px;"><div class="local-main"><i class="fas fa-star local-star"></i></div><div class="local-content">সদস্য</div></div>
-												<div><span style="margin-left:5px;font-size:14px;">Dhaka,</span></div>
-												<div><span style="margin-left:5px;font-size:14px;">Electronics</span></div>
-												</div>
-												<div class="price-details clearfix mt-3">
-													<div class="price-number float-left">
-														<p class="text-left" style="font-size:20px"><strong class="rupees">{{$prod->price}} BDT</strong></p>
-													</div>
-													<div class="add-cart float-right">
-															<span style="background: red;" class="add-to-cart add-to-cart-btn" data-href="{{ route('product.cart.add',$prod->id) }}">
-																	{{ $langg->lang56 }}
-																</span>
-																<!-- <span class="add-to-cart-quick add-to-cart-btn"
-																	data-href="{{ route('product.cart.quickadd',$prod->id) }}">
-																	<i class="icofont-cart"></i> {{ $langg->lang251 }}
-																</span> -->
-													</div>
-													<div class="clear"></div>
-												</div>
-											</div>
-										</div>
-										<!-- <a href="{{ route('front.product', $prod->slug) }}" class="item">
+														</span> @else
+                            <span href="javascript:;" rel-toggle="tooltip" title="" data-toggle="modal" id="wish-btn" data-target="#comment-log-reg" data-placement="right" data-original-title="Add To Wishlist">
+															<i class="icofont-heart-alt"></i>
+														</span> @endif
+                        </li>
+                    </ul>
+                </div>
+                <a href="product-details.html">
+                    <div class="product-thum-img">
+                        <a href="{{URL::to('/item/'.$prod->slug)}}"><img src="{{ $prod->photo ? asset('assets/images/thumbnails/'.$prod->thumbnail):asset('assets/images/noimage.png') }}" alt="" /></a>
+                    </div>
+                </a>
+                <h5 class="mt-2">{{$prod->name}}</h5>
+                <div class="local-info d-flex flex-row flex-wrap">
+                    <div class="d-flex" style="margin-top:5px;">
+                        <div class="local-main"><i class="fas fa-star local-star"></i></div>
+                        <div class="local-content">Member</div>
+                    </div>
+                    <div><span style="margin-left:5px;font-size:14px;">{{$prod->area?$prod->area->name:''}},</span></div>
+                    <div><span style="margin-left:5px;font-size:14px;">{{$prod->category->name}}</span></div>
+                    <div class="ml-auto mt-2" style="font-size:14px">
+                        {{$prod->created_at->diffForHumans()}}
+                    </div>
+                </div>
+                <div class="price-details clearfix mt-3">
+                    <div class="price-number float-left">
+                        <p class="text-left" style="font-size:20px"><strong class="rupees">{{$prod->price}} BDT</strong></p>
+                    </div>
+                    <div class="add-cart float-right">
+                        <a style="background: red;" class="add-to-cart-a " href="{{ route('product.cart.quickadd',$prod->id) }}">
+																							Buy Now
+																						</a>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+        <!-- <a href="{{ route('front.product', $prod->slug) }}" class="item">
 											<div class="item-img">
 												@if(!empty($prod->features))
 													<div class="sell-area">
@@ -123,56 +131,49 @@
 											</div>
 										</a> -->
 
-									</div>
-				
-				@endforeach
-				</div>
-				<div class="bottom-add mt-3">
-					<div class="container">
-						<center><img src="{{ asset('assets/images/brand/gp.gif')}}"></center>
-					</div>
-				</div>
-				<div class="col-lg-12 pb-4">
-					<div class="page-center mt-5">
-						{!! $prods->appends(['search' => request()->input('search')])->links() !!}
-					</div>
-				</div>
-			@else
-				<div class="col-lg-12 pb-4">
-					<div class="page-center">
-						 <h4 class="text-center">{{ $langg->lang60 }}</h4>
-					</div>
-				</div>
-			@endif
+    </div>
 
-
-@if(isset($ajax_check))
+    @endforeach
+</div>
+<div class="bottom-add mt-3">
+    <div class="container">
+        <center><img src="{{ asset('assets/images/brand/gp.gif')}}"></center>
+    </div>
+</div>
+<div class="col-lg-12 pb-4">
+    <div class="page-center mt-5">
+        {!! $prods->appends(['search' => request()->input('search')])->links() !!}
+    </div>
+</div>
+@else
+<div class="col-lg-12 pb-4">
+    <div class="page-center">
+        <h4 class="text-center">{{ $langg->lang60 }}</h4>
+    </div>
+</div>
+@endif @if(isset($ajax_check))
 
 
 <script type="text/javascript">
+    // Tooltip Section
 
 
-// Tooltip Section
-
-
-    $('[data-toggle="tooltip"]').tooltip({
-      });
-      $('[data-toggle="tooltip"]').on('click',function(){
-          $(this).tooltip('hide');
-      });
+    $('[data-toggle="tooltip"]').tooltip({});
+    $('[data-toggle="tooltip"]').on('click', function() {
+        $(this).tooltip('hide');
+    });
 
 
 
 
-      $('[rel-toggle="tooltip"]').tooltip();
+    $('[rel-toggle="tooltip"]').tooltip();
 
-      $('[rel-toggle="tooltip"]').on('click',function(){
-          $(this).tooltip('hide');
-      });
+    $('[rel-toggle="tooltip"]').on('click', function() {
+        $(this).tooltip('hide');
+    });
 
 
-// Tooltip Section Ends
-
+    // Tooltip Section Ends
 </script>
 
 @endif
